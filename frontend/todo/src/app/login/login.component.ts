1a320a5da19223b831d60202786d27be3a222817
yasiruPadmasiri <66236929+yasiruPadmasiri@users.noreply.github.com>
2020-11-18 20:34:10 +0530

    change again loging add error fixed

--- frontend/todo/src/app/login/login.component.ts
+++ frontend/todo/src/app/login/login.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  
+  username='yasiruPadmasiri'
+  password=''
+
   constructor() { }
 
   ngOnInit(): void {
