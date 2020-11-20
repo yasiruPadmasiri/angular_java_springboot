1e0c6bfb8eb50446912849e783c9510767c113fc
yasiruPadmasiri <66236929+yasiruPadmasiri@users.noreply.github.com>
2020-11-20 17:15:07 +0530

    addd error component

--- frontend/todo/src/app/app-routing.module.ts
+++ frontend/todo/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './login/login.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
+  { path:'',component:LoginComponent},
   { path:'login',component:LoginComponent},
   { path:'welcome',component:WelcomeComponent}
 ];
