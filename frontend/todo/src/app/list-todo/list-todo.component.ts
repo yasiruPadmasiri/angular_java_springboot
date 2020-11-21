0c81b6fd757908b527b35bff4f0862befc0f87c3
yasiruPadmasiri <66236929+yasiruPadmasiri@users.noreply.github.com>
2020-11-21 21:00:25 +0530

    suucuss full  work Todos objects displayed

--- frontend/todo/src/app/list-todo/list-todo.component.ts
+++ frontend/todo/src/app/list-todo/list-todo.component.ts
@@ -1,17 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Todo{
+  constructor(
+    public id: number,
+    public description : string,
+    public done:boolean,
+    public targetDate :Date
+
+  ){
+
+  }
+}
 @Component({
   selector: 'app-list-todo',
   templateUrl: './list-todo.component.html',
   styleUrls: ['./list-todo.component.css']
 })
+
+
 export class ListTodoComponent implements OnInit {
 
   todos = [
-    {id:1 , description:'Learn to fight'},
-    {id:2,description:'Learn  Try to Defeat'},
-    {id:3,description:'Learn to Escape'},
-    {id:4,description:'Learn to fight with Tricks '}
+    new Todo(1,'study the enemy',false,new Date()),
+    new Todo(2,'study fight style',false,new Date()),
+    new Todo(3,'Learn to fight',false,new Date()),
+    new Todo(4,'Learn to Escape',false,new Date()),
+    new Todo(5,'Learn to fight with Tricks',false,new Date()),
+    new Todo(5,'Learn to fight with Backupm Plan',false,new Date()),
+    // {id:1 , description:'Learn to fight'},
+    // {id:2,description:'Learn  Try to Defeat'},
+    // {id:3,description:'Learn to Escape'},
+    // {id:4,description:'Learn to fight with Tricks '}
   ]
   // todo={
   //   id:1,
