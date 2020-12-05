b3482d27377531f835e7bf7b8d56d96cdbd335c5
yasiruPadmasiri <66236929+yasiruPadmasiri@users.noreply.github.com>
2020-12-05 22:56:09 +0530

    rest api retrieving  function working

--- restful-webservice/src/main/java/com/yasiuruPadmasiri/rest/webservice/restfulwebservice/todo/TodoHardcodedService.java
+++ restful-webservice/src/main/java/com/yasiuruPadmasiri/rest/webservice/restfulwebservice/todo/TodoHardcodedService.java
@@ -1,5 +1,28 @@
 package com.yasiuruPadmasiri.rest.webservice.restfulwebservice.todo;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+
+
+@Service
 public class TodoHardcodedService {
+	private static List<Todo> todos=new ArrayList<Todo>();
+	private static int idCounter=0;
+	 
+	static {
+		
+		todos.add(new Todo(++idCounter,"in yasiruPadmasiri","learn to fight",new Date(),false));
+		todos.add(new Todo(++idCounter,"in yasiruPadmasiri","learn to Node js",new Date(),false));
+		todos.add(new Todo(++idCounter,"in yasiruPadmasiri","learn to Angular",new Date(),false));
+		todos.add(new Todo(++idCounter,"in yasiruPadmasiri","learn to Springboot",new Date(),false));
+ 
+	}
+	public List<Todo> findAll(){
+		return todos; 
+	}
 
 }
--- restful-webservice/src/main/java/com/yasiuruPadmasiri/rest/webservice/restfulwebservice/todo/TodoResource.java
+++ restful-webservice/src/main/java/com/yasiuruPadmasiri/rest/webservice/restfulwebservice/todo/TodoResource.java
@@ -1,12 +1,22 @@
  package com.yasiuruPadmasiri.rest.webservice.restfulwebservice.todo;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yasiuruPadmasiri.rest.webservice.restfulwebservice.todo.Todo;
 
 @RestController
 public class TodoResource {
-	public List<Todo> getAlltodos(String username){
+	@Autowired
+	private TodoHardcodedService  todoService;
+	@GetMapping("/users/{username}/todos")
+	public List<Todo> getAlltodos(@PathVariable String username){
+		return todoService.findAll(); 
 		
 	}
 }
+ 
